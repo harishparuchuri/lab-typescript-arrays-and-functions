b42e0cc5aefc7b4b0eb069e7f6336c37e5c9bb66
harishparuchuri <harishparuchuri007@gmail.com>
2020-04-22 14:41:02 +0530

    done

--- main.js
+++ main.js
@@ -1,63 +1,62 @@
 "use strict";
 exports.__esModule = true;
-var ArrayComponent = /** @class */ (function () {
-    function ArrayComponent() {
+var ArrayManipulations = /** @class */ (function () {
+    function ArrayManipulations() {
     }
-    ArrayComponent.prototype.arrayMultiply = function (myArray) {
-        var temp = [], j = 0;
+    ArrayManipulations.prototype.arrayFindMultiples = function (myArray) {
+        var arr = [];
+        var j = 0;
         for (var i = 0; i < myArray.length; i++) {
-            if (myArray[i] % 5 === 0 || myArray[i] % 10 === 0) {
-                temp[j] = myArray[i];
-                j++;
+            if (myArray[i] % 5 == 0 || myArray[i] % 10 == 0) {
+                arr[j++] = myArray[i];
             }
         }
-        return temp;
+        return arr;
     };
-    ArrayComponent.prototype.arraySeparate = function (myArray) {
-        var str = [], j = 0;
+    ArrayManipulations.prototype.arraySeparate = function (myArray) {
+        var arr = [], j = 0;
         for (var i = 0; i < myArray.length; i++) {
-            if (typeof (myArray[i]) === "string") {
-                str[j] = myArray[i];
-                j++;
+            if (typeof myArray[i] == 'string') {
+                arr[j++] = myArray[i];
             }
         }
-        return str;
+        return arr;
     };
-    ArrayComponent.prototype.arraySplit = function (str) {
-        var temp = str.split(",");
-        var primeArray = [];
+    ArrayManipulations.prototype.arraySplit = function (str) {
+        var arr = str.split(",");
+        var prime = [];
         var k = 0;
-        for (var i = 0; i < temp.length; i++) {
-            var y = +temp[i];
+        for (var i = 0; i < arr.length; i++) {
+            var y = +arr[i];
             for (var j = 2; j < y; j++) {
                 if (y % j === 0)
                     continue;
                 else {
-                    primeArray[k] = y;
+                    prime[k] = y;
                     k++;
                 }
             }
         }
-        return primeArray;
+        return prime;
     };
-    ArrayComponent.prototype.arraySort = function (myArray) {
+    ArrayManipulations.prototype.arraySort = function (myArray) {
         myArray.sort();
-        myArray.reverse();
         return myArray;
     };
-    ArrayComponent.prototype.arrayReplace = function (myArray) {
+    ArrayManipulations.prototype.arrayReplace = function (myArray) {
         for (var i = 0; i < myArray.length; i++) {
-            if (myArray[i] % 3 === 0)
+            if (myArray[i] % 3 === 0) {
                 myArray[i] = 5;
+            }
         }
         return myArray;
     };
-    return ArrayComponent;
+    return ArrayManipulations;
 }());
-exports.ArrayComponent = ArrayComponent;
-var myArray = [34, 45, 60, 23, 13, 25, 70];
-var array = new ArrayComponent();
-console.log(array.arrayMultiply(myArray));
-console.log(array.arraySeparate(myArray));
-console.log(array.arraySort(myArray));
-console.log(array.arrayReplace(myArray));
+exports.ArrayManipulations = ArrayManipulations;
+var myArray = [25, 87, 45, 14, 36, 45, 14, 28, 49];
+var array = new ArrayManipulations();
+console.log("Array multiples" + array.arrayFindMultiples(myArray));
+console.log("Array separate" + array.arraySeparate(myArray));
+console.log("Array Sort" + array.arraySort(myArray));
+console.log("Array replace" + array.arrayReplace(myArray));
--- main.ts
+++ main.ts
@@ -1,70 +1,70 @@
-import { ArrayService } from "./app-service";
+import { StringManipulationService } from "./app-service";
+export class ArrayManipulations implements StringManipulationService{
 
-export class ArrayComponent implements ArrayService {
+    constructor() { }
 
-    constructor() {}
-
-    public arrayMultiply(myArray: number[]) : Array<Number> {
-        let temp = [], j=0;
-        for(var i=0;i<myArray.length;i++){
-            if(myArray[i]%5===0 || myArray[i]%10 ===0){
-                temp[j] = myArray[i];
-                j++;
-            }              
-        }
-        return temp
-    }
-
-    public arraySeparate(myArray: any) : Array<string>{
-        let str = [], j=0;
-        for(var i=0;i<myArray.length;i++){
-            if(typeof(myArray[i]) === "string"){
-                str[j] = myArray[i];
-                j++;
-            }              
-        }
-        return str;
-    }
-
-    public arraySplit(str:string) : Array<number>{
-        const temp = str.split(",");
-        const primeArray = [];
-        let k=0;
-        for(var i=0;i<temp.length;i++){
-            let y = +temp[i];
-            for(var j=2;j<y;j++){
-                if(y%j===0)
-                    continue;
-                else{
-                    primeArray[k] = y;
-                    k++; }
+    public arrayFindMultiples(myArray: any): Array<Number>{
+        let arr=[];
+        let j=0;
+        for(let i=0;i<myArray.length;i++)
+        {
+            if(myArray[i]%5==0 || myArray[i]%10==0)
+            {
+                arr[j++]=myArray[i];
             }
         }
-        return primeArray;
+        return arr;
+    }
+    public arraySeparate(myArray: any): Array<string>{
+        let arr=[],j=0;
+        for(var i=0;i<myArray.length;i++)
+        {
+            if(typeof myArray[i]=='string')
+            {
+                arr[j++]=myArray[i];
+            }
+        }
+        return arr;
+
     }
 
-    public arraySort(myArray:any):Array<string>{
+    public arraySplit(str: string): Array<number> {
+        const arr = str.split(",");
+        const prime = [];
+        let k = 0;
+        for (var i = 0; i < arr.length; i++) {
+            let y = +arr[i];
+            for (var j = 2; j < y; j++) {
+                if (y % j === 0) continue;
+                else {
+                    prime[k] = y;
+                    k++;
+                }
+            }
+        }
+        return prime;
+    }
+
+    public arraySort(myArray: any): Array<string> {
         myArray.sort();
-        myArray.reverse();
         return myArray;
     }
 
-    public arrayReplace(myArray:any):Array<number>{
-        for(var i=0;i<myArray.length;i++){
-            if(myArray[i]%3===0)
+    public arrayReplace(myArray: any): Array<number> {
+        for (var i = 0; i < myArray.length; i++) {
+            if (myArray[i] % 3 === 0)
+            {
                 myArray[i] = 5;
+            } 
         }
         return myArray;
     }
 }
 
-let myArray: number[] = [34, 45, 60, 23, 13, 25, 70];
-let array = new ArrayComponent();
+let myArray: number[] = [25,87,45,14,36,45,14,28,49];
+let array = new ArrayManipulations();
 
-console.log(array.arrayMultiply(myArray));
-console.log(array.arraySeparate(myArray));
-console.log(array.arraySort(myArray));
-console.log(array.arrayReplace(myArray));
-
-
-
+console.log("Array multiples"+array.arrayFindMultiples(myArray));
+console.log("Array separate"+array.arraySeparate(myArray));
+console.log("Array Sort"+array.arraySort(myArray));
+console.log("Array replace"+array.arrayReplace(myArray));
